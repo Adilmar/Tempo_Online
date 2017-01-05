9d2f484a4a0d3bec98f8c191405d3c3f8858a0ea
Adilmar Dantas <Adilmar Dantas>
2017-01-05 20:28:08 -0200

    tempo online api

---
+++ app/src/main/java/com/fenix/temperatura/Main.java
@@ -0,0 +1,126 @@
+package com.fenix.temperatura;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+public class Main extends AppCompatActivity {
+
+    String url = "http://api.apixu.com/";
+    TextView text_id_1, text_name_1, text_marks_1 ;
+    TextView text_id_2, text_name_2, text_marks_2 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        text_id_1 = (TextView) findViewById(R.id.text_id_1);
+        text_name_1 = (TextView) findViewById(R.id.text_name_1);
+        text_marks_1 = (TextView) findViewById(R.id.text_marks_1);
+
+        text_id_2 = (TextView) findViewById(R.id.text_id_2);
+        text_name_2 = (TextView) findViewById(R.id.text_name_2);
+        text_marks_2 = (TextView) findViewById(R.id.text_marks_2);
+
+
+        Button ButtonObject= (Button) findViewById(R.id.RetrofitObject);
+
+
+
+        ButtonObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View VisibleArray = findViewById(R.id.RetrofitArray);
+                VisibleArray.setVisibility(View.GONE);
+                View VisibleObject = findViewById(R.id.RetrofitObject);
+                VisibleObject.setVisibility(View.GONE);
+                getRetrofitObject();
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+
+    void getRetrofitObject() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitObjectAPI service = retrofit.create(RetrofitObjectAPI.class);
+
+        Call<Current> call = service.getCurrentDetails();
+
+        call.enqueue(new Callback<Current>() {
+            @Override
+            public void onResponse(Response<Current> response, Retrofit retrofit) {
+
+                try {
+
+                    text_id_1.setText("Temperatura  :  " + response.body().getTemp_c());
+                    Log.d("sucesso", "sucesso");
+
+                } catch (Exception e) {
+                    Log.d("onResponse", "There is an error");
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.d("onFailure", t.toString());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
